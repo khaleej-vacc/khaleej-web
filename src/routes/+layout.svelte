d99f828ad16f1dc8a5be3a1f4c58eef6825fa2f0
Abdulrahman <86178679+ARace863@users.noreply.github.com>
2026-06-16 08:08:44 +0400

    Small big fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,14 +9,14 @@
 
 <svelte:head>
 	<title>Khaleej vACC</title>
-	<meta name="description" content="Khaleej vACC is a virtual area control center for the Bahrain and Kuwait region on the VATSIM network." />
+	<meta name="description" content="Khaleej vACC is a virtual area control center for the Bahrain and Qatar region on the VATSIM network." />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="theme-color" content="#d8c8a0" />
 
 	<!-- OpenGraph -->
 	<meta name="og:type" content="website" />
 	<meta name="og:title" content="Khaleej vACC" />
-	<meta name="og:description" content="Khaleej vACC is a virtual area control center for the Bahrain and Kuwait region on the VATSIM network." />
+	<meta name="og:description" content="Khaleej vACC is a virtual area control center for the Bahrain and Qatar region on the VATSIM network." />
 	<meta name="og:image" content="https://khaleejvacc.net/favicon-96x96.png" />
 	<meta name="og:url" content="https://khaleejvacc.net" />
 </svelte:head>
